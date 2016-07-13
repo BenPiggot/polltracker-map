d136e81f46d08acf7fc9d731897973d6d7c33223
wbp <wbpiggot@gmail.com>
2016-07-13 16:21:28 -0700

    wip

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import MapPaths from './state-data.js';
+import ScoreboardFixture from './scoreboard-fixture.js';
 import _ from 'lodash';
 import State from './State.js';
 
@@ -8,7 +9,8 @@ class MapContainer extends Component {
   render() {
     const states = _.reduce(MapPaths.paths, (memo, v, k) => {
       const boxDimensions = _.includes(Object.keys(MapPaths.callouts), k) ? MapPaths.callouts[k] : null;
-      return memo.concat(<State stateName={k} path={v} boxDimensions={boxDimensions} />)
+      const data = _.includes(Object.keys(ScoreboardFixture.states), k) ? ScoreboardFixture.states[k] : null;
+      return memo.concat(<State stateName={k} path={v} boxDimensions={boxDimensions} data={data} />)
     }, [])
 
     return <div>
--- app/State.js
+++ app/State.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+
 class State extends Component {
+  status(stateData) {
+    switch(stateData) {
+      case 'Tossup':
+        return '#f2e6c4'
+      case 'Leans Clinton':
+        return '#bdcee1'
+      case 'Favors Clinton':
+        return '#6a84b5'
+      case 'Strongly Clinton':
+        return 
+      case 'Leans Trump':
+        return '#bdcee1'
+      case 'Favors Trump':
+        return
+      case 'Strongly Trump':
+        return '#9f0000'
+      default:
+        return 'lightblue'
+    }
+  }
+
+  handleMouseOver(event) {
+    console.log(event)
+  }
+
   render() {
+    const data = this.props.data ? this.props.data.classification : null; 
     if (!this.props.boxDimensions)
-      return <path style={{fill: 'lightblue', stroke: 'white'}} className={this.props.stateName} d={this.props.path} strokeWidth={1} strokeLinejoin="round"></path>
+      return <path style={{fill: this.status(data), stroke: 'white'}} 
+        onMouseOver={this.handleMouseOver}
+        className={this.props.stateName} 
+        d={this.props.path} strokeWidth={1} 
+        strokeLinejoin="round"> 
+      </path>
     else {
-      console.log(this.props.stateName)
       return <g>
-        <path style={{fill: 'blue', stroke: 'white'}} className={this.props.stateName} d={this.props.path} strokeWidth={1} strokeLinejoin="round"></path>
-        <rect style={{fill: 'white', stroke: '#aaa'}} height="20" width="50" x="950" y={this.props.boxDimensions.y2}></rect>
-        <text x="950" y={parseInt(this.props.boxDimensions.y2) + 10} style={{fontSize: 10, fontFamily: 'helvetica'}}>{this.props.stateName}</text>
+        <path style={{fill: this.status(data), stroke: 'white'}} 
+          onMouseOver={this.handleMouseOver}
+          className={this.props.stateName} 
+          d={this.props.path} strokeWidth={1} 
+          strokeLinejoin="round">
+        </path>
+        <rect style={{fill: 'white', stroke: '#aaa'}} height="20" width="50" x="950" 
+          y={this.props.boxDimensions.y2}>
+        </rect>
+        <text x="950" y={parseInt(this.props.boxDimensions.y2) + 10} 
+          style={{fontSize: 10, fontFamily: 'helvetica'}}>{this.props.stateName}
+        </text>
       </g>
     }
   }
